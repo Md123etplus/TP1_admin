0b2fb198c100245e426c467092f4ca9eec809c2f
RajaeBoutayeb <rajaeboutayeb6@gmail.com>
2025-03-05 12:09:38 +0100

    Ajout du dossier BD

--- BD/Utilisateur.php
+++ BD/Utilisateur.php
@@ -1,34 +1,67 @@
 <?php
 require_once 'Connexion.php';
-function getAllUsers(){
+
+
+
+
+function getAllUsers() {
     $bdd = connexion();
-    $sql="SELECT * FROM users";
+    $sql = "SELECT * FROM users";
     $stmt = $bdd->prepare($sql);
     $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $result;
-    // return $users;
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-function getUserById($id){
-    $bdd=connexion();
-    $sql="SELECT * FROM users WHERE id=?";
-    $stmt=$bdd->prepare($sql);
-    $stmt->execute([$id]);
-    $result=$stmt->fetch(PDO::FETCH_ASSOC);
-    return $result;
-    
-    // return $user;
+
+//récupérer un utilisateur par son ID
+function getUserById($id) {
+    $bdd = connexion();
+    $sql = "SELECT * FROM users WHERE id = :id";
+    $stmt = $bdd->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC); // Retourne un seul utilisateur
 }
-function deleteUserById($id){
-    $bdd=connexion();
-    $sql="DELETE FROM users WHERE id=?";
-    $stmt=$bdd->prepare($sql);
+
+
+function deleteUser($id) {
+    $bdd = connexion();
+    $sql = "DELETE FROM users WHERE id = ?";
+    $stmt = $bdd->prepare($sql);
     $stmt->execute([$id]); 
-    return $stmt->rowCount();
-    // $stmt=mysqli_prepare($bdd,$sql);
-    // mysqli_stmt_bind_param($stmt,'i',$id);
-    // mysqli_stmt_execute($stmt);
-    // return mysqli_stmt_affected_rows($stmt);
+    return $stmt->rowCount(); 
 }
+
+function addUser( $nom, $prenom, $email, $age, $filiere, $annee){
+    $bdd = connexion();
+    $sql = "Insert into users  (nom , prenom , email ,age ,filiere, annee) values( :nom , :prenom ,:email ,:age ,:filiere ,:annee) ";
+    $stmt = $bdd->prepare($sql);
+    $stmt->bindParam(':nom', $nom);
+    $stmt->bindParam(':prenom', $prenom);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':age', $age);
+    $stmt->bindParam(':filiere', $filiere);
+    $stmt->bindParam(':annee', $annee);
+    return $stmt->execute();
+}
+
+function updateUser($id, $nom, $prenom, $email, $age, $filiere, $annee) {
+    $bdd = connexion();
+    $sql = "UPDATE users 
+            SET nom = :nom, prenom = :prenom, email = :email, 
+                age = :age, filiere = :filiere, annee = :annee 
+            WHERE id = :id";
+
+    $stmt = $bdd->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':nom', $nom);
+    $stmt->bindParam(':prenom', $prenom);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':age', $age, PDO::PARAM_INT);
+    $stmt->bindParam(':filiere', $filiere);
+    $stmt->bindParam(':annee', $annee, PDO::PARAM_INT);
+
+    return $stmt->execute(); 
+}
+
 
 ?>
--- Traitement/Utilisateurs.php
+++ Traitement/Utilisateurs.php
@@ -1,28 +1,35 @@
 <?php
-require_once 'BD/Utilisateur.php';
+define('ROOT',str_replace('Traitement\Utilisateurs.php','',$_SERVER['SCRIPT_FILENAME']));
+require_once ROOT.'BD\Utilisateur.php';
+// print_r(ROOT);
 if (empty($_POST)&& empty($_GET)) {
     $users = getAllUsers();
     // $_SESSION['users'] = $users;
     // header('Location: ');
-    include('IHM/utilisateur/index.php');
+    // include('..\IHM\utilisateur\index.php');
+    include(ROOT.'IHM\utilisateur\index.php');
     exit();
 }
 else if(isset($_GET['action'])){
     $action=$_GET['action'];
     switch($action){
-        case 'edit':
+        case 'modifier':
             $user=getUserById($_GET['data']);
             if($user){
-                include('IHM/utilisateur/form_edit.php');
+                // include('..\IHM\utilisateur\form_edit.php');
+                include(ROOT.'IHM\utilisateur\form_edit.php');
             }else{
                 echo "Utilisateur non trouvé";
             }
             break;
-        case 'delete':
+        case 'supprimer':
             $user=deleteUserById($_GET['data']);
-            if($user>0){
+            $isDeleted=false;
+            if($user){
+                $isDeleted=true;
                 $users = getAllUsers();
-                include('IHM/utilisateur/index.php');
+                include(ROOT.'IHM\utilisateur\index.php');
+                // include('..\IHM\utilisateur\index.php');
             }else{
                 echo "Erreur de suppression";
             }
@@ -35,6 +42,38 @@ else if(isset($_GET['action'])){
             echo "Action non reconnue";
     }
 }
-else if(isset($_POST['submit'])){
-    
+else if(isset($_POST['submit_add'])){
+    $nom=$_POST['nom'];
+    $prenom=$_POST['prenom'];
+    $email=$_POST['email'];
+    $apropos=$_POST['apropos'];
+    $age=$_POST['age'];
+    $tel=$_POST['tel'];
+    // $user=addUser($nom,$prenom,$email,$apropos,$age,$tel);
+    if($user){
+        $users = getAllUsers();
+        include(ROOT.'IHM\utilisateur\index.php');
+        // include('..\IHM\utilisateur\index.php');
+    }else{
+        echo "Erreur d'ajout";
+    }
+}
+else if(isset($_POST['submit_edit'])){
+    $nom=$_POST['nom'];
+    $prenom=$_POST['prenom'];
+    $email=$_POST['email'];
+    $apropos=$_POST['apropos'];
+    $age=$_POST['age'];
+    $tel=$_POST['tel'];
+    // $user=updateUser($id,$nom,$prenom,$email,$apropos,$age,$tel);
+    if($user>0){
+        $users = getAllUsers();
+        include(ROOT.'IHM\utilisateur\index.php');
+        // include('..\IHM\utilisateur\index.php');
+    }else{
+        echo "Erreur de modification";
+    }
+}
+else{
+    echo "Action non reconnue";
 }
